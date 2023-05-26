50abf40b97f05623ae4b657f878bde577f7c3348
Sergio <sgarnica1902@gmail.com>
2023-05-25 22:54:38 -0600

    Update everythtin, add concat

--- src/automata.cpp
+++ src/automata.cpp
@@ -5,416 +5,265 @@
 #include "automata.h"
 #include "graph.cpp"
 
-// Regular expressions
-std::string operators = "[\\+\\|\\*]";
-std::string expressions = "[a-zA-Z0-9]";
+std::string OPERATORS = "[\\+\\|\\*\\-]";
+std::string OPERANDS = "[a-zA-Z0-9]";
+std::string CONCAT_OP = "-";
 
-// Regex patterns
-std::regex operPattern(operators);
-std::regex exprPattern(expressions);
+std::regex operatorsPattern(OPERATORS);
+std::regex operandsPattern(OPERANDS);
 
-// Read methods
-
+// Automata methods
 /**
  * @brief
- * Reads an expression and adds it to the stack
- * @param expression expression to be read
- * @return void
+ * Read the expression
+ * @param expression
+ * The expression to read
  */
-
 void Automata::readExpression(std::string expression)
 {
-  // std::cout << "Expression: " << expression << "\n\n";
+  std::cout << "Expression: " << expression << std::endl;
 
-  this->prev = "";
-
-  for (char character : expression)
+  for (char char_ : expression)
   {
-    std::string char_(1, character);
+    std::string character(1, char_);
 
-    if (char_ == "+")
-      oneOrMore();
-    if (char_ == "*")
-      zeroOrMore();
-      
-    if (getLastOperator() == "|")
+    // Match operands
+    if (std::regex_match(character, operandsPattern))
+      pushAutomata(Graph(character));
+
+    // Match opening parenthesis
+    if (character == "(")
+      pushOperator(character);
+
+    // Match operators
+    if (std::regex_match(character, operatorsPattern))
     {
-      int Node1 = graph.createNode();
-      int Node2 = graph.createNode();
+      // If operators stack is empty, add the operator
+      if (operators.size() == 0)
+      {
+        pushOperator(character);
+        continue;
+      }
 
-      // Create new connection between Node1 and Node2 through weight
-      this->graph.addEdge(std::to_string(Node1), std::to_string(Node2), char_);
-
-      or_(std::to_string(Node1), std::to_string(Node2));
-      continue;
+      // If it is not empty, compare current op presedence with last op in stack
+      if (presedence(character) > presedence(getOperator()))
+        pushOperator(character);
+      else
+      {
+        std::string lastOp = getOperator();
+        popOperator();
+        applyOperator(lastOp);
+        pushOperator(character);
+      }
     }
 
-    // Check if the character is an expression
-    if (graph.size() > 0 && std::regex_match(char_, exprPattern))
-      concat(char_);
-
-    // Check if the character is the first expression
-    if (graph.size() == 0 && std::regex_match(char_, exprPattern))
-      setFirstEdge(char_);
-
-    // Check if the character is an operator
-    if (std::regex_match(char_, operPattern))
+    if (character == ")")
     {
-      this->addOperator(char_);
-    }
-
-    this->prev = char_;
-  }
-
-  graph.printAdjList();
-  std::cout << "\n";
-  printPositionStack();
-  std::cout << "\n";
-}
-
-// Access methods
-
-/**
- * @brief
- * Gets the last operator of the stack
- * @return std::string Last operator of the stack
- */
-
-std::string Automata::getLastOperator()
-{
-  if (this->operatorStack.empty())
-    return "";
-  return this->operatorStack.top();
-}
-
-/**
- * @brief
- * Gets the temporary head node
- * @return std::string Temporary head node
- */
-std::string Automata::getTempHeadNode()
-{
-  return this->tempHeadNode;
-}
-
-/**
- * @brief
- * Gets the last position of the stack
- * @return std::pair<std::string, std::string> Last position of the stack
- */
-std::string Automata::getLastPosition()
-{
-  if (this->positionStack.empty())
-    return "";
-  return this->positionStack.top();
-}
-
-/**
- * @brief
- * Gets the previous value
- * @return std::string Previous value
- */
-std::string Automata::getPrev()
-{
-  return this->prev;
-}
-
-// Setters
-
-/**
- * @brief
- * Sets the temporary head node
- * @param tempHeadNode Temporary head node
- * @return void
- */
-
-void Automata::setTempHeadNode(std::string tempHeadNode)
-{
-  this->tempHeadNode = tempHeadNode;
-}
-
-/**
- * @brief
- * Set previous value
- * @param prev Previous value
- * @return void
- */
-
-void Automata::setPrev(std::string prev)
-{
-  this->prev = prev;
-}
-
-// Manipulation methods
-/**
- * @brief
- * Gets the last operator of the stack
- * @return std::string Last operator of the stack
- */
-void Automata::popOperator()
-{
-  this->operatorStack.pop();
-}
-
-/**
- * @brief
- * Gets the last expression of the stack
- * @return std::string Last expression of the stack
- */
-void Automata::popExpression()
-{
-  this->expressionStack.pop();
-}
-
-/**
- * @brief
- * Gets the last position of the stack
- * @return std::string Last position of the stack
- */
-void Automata::popPosition()
-{
-  this->positionStack.pop();
-}
-
-/**
- * @brief
- * Adds an expression to the stack
- * @param expression expression to be added
- * @return void
- */
-void Automata::addPosition(std::string position)
-{
-  this->positionStack.push(position);
-}
-
-/**
- * @brief
- * Adds an operator to the stack
- * @param operator_ Operator to be added
- * @return void
- */
-void Automata::addOperator(std::string operator_)
-{
-  this->operatorStack.push(operator_);
-}
-
-/**
- * @brief
- * Adds an expression to the stack
- * @param expression expression to be added
- * @return void
- */
-
-void Automata::addExpression(std::string expression)
-{
-  this->expressionStack.push(expression);
-}
-
-// Print methods
-
-/**
- * @brief
- * Prints the operator stack
- * @return void
- */
-void Automata::printOperatorStack()
-{
-  std::stack<std::string> tempStack;
-  tempStack = this->operatorStack;
-
-  std::cout << "Operator stack: ";
-
-  while (!tempStack.empty())
-  {
-    std::cout << tempStack.top() << " ";
-    tempStack.pop();
-  }
-}
-
-/**
- * @brief
- * Prints the expression stack
- * @return void
- */
-
-void Automata::printExpressionStack()
-{
-  std::stack<std::string> tempStack;
-  tempStack = this->expressionStack;
-
-  std::cout << "Expression stack: ";
-
-  while (!tempStack.empty())
-  {
-    std::cout << tempStack.top() << " ";
-    tempStack.pop();
-  }
-}
-
-/**
- * @brief
- * Prints the position stack
- * @return void
- */
-
-void Automata::printPositionStack()
-{
-  std::stack<std::string> tempStack;
-  tempStack = this->positionStack;
-
-  std::cout << "Position stack: ";
-
-  while (!tempStack.empty())
-  {
-    std::cout << tempStack.top() << " ";
-    tempStack.pop();
-  }
-}
-
-// Operator helpers
-
-/**
- * @brief
- * Sets the first node of the graph
- * @param character character to be added
- * @return void
- */
-void Automata::setFirstEdge(std::string character)
-{
-  // Create two nodes
-  int Node1 = this->graph.createNode();
-  int Node2 = this->graph.createNode();
-
-  // Set node 1 as head
-  this->graph.setHead(std::to_string(Node1));
-  setTempHeadNode(std::to_string(Node1));
-
-  // Set node 2 as tail
-  this->graph.setTail(std::to_string(Node2));
-
-  // Connect node 1 to node 2 through character
-  this->graph.addEdge(std::to_string(Node1), std::to_string(Node2), character);
-
-  if (this->prev == "(")
-    addPosition(std::to_string(Node1));
-}
-
-/**
- * @brief
- * Concatenates the last two expressions
- * @param character character to be concatenated
- * @return void
- */
-void Automata::concat(std::string character)
-{
-  // Create a new node
-  int Node = this->graph.createNode();
-
-  // Connect new node to last node
-  setTempHeadNode(this->graph.getTail());
-  this->graph.addEdge(this->graph.getTail(), std::to_string(Node), character);
-
-  // Set new node as tail
-  this->graph.setTail(std::to_string(Node));
-
-  if (this->prev == "(")
-    addPosition(getTempHeadNode());
-}
-
-/**
- * @brief
- * Allows a character to be repeated
- * @return void
- */
-void Automata::oneOrMore()
-{
-  this->graph.addEdge(this->graph.getTail(), getTempHeadNode(), "ε");
-}
-
-/**
- * @brief
- * Zero or more. Allows a character to be repeated or not
- * @return void
- */
-
-void Automata::zeroOrMore()
-{
-  oneOrMore();
-
-  // Create 2 new nodes
-  int origin = this->graph.createNode();
-  int destiny = this->graph.createNode();
-
-  // Verify if a node is already connected to the temp head
-  bool found = false;
-  for (auto &i : graph.getAdjList())
-  {
-    for (auto &j : i.second)
-    {
-      if (std::stoi(i.first) == std::stoi(getTempHeadNode()))
-        found = true;
-
-      if (getTempHeadNode() == j.first && found)
-        graph.modifyDestiny(i.first, j.first, j.second, std::to_string(origin));
+      while (operators.top() != "(")
+      {
+        std::string lastOp = getOperator();
+        popOperator();
+        applyOperator(lastOp);
+      }
     }
   }
 
-  // Set origin as head
-  if (getTempHeadNode() == graph.getHead())
-    graph.setHead(std::to_string(origin));
+  while (!operators.empty())
+  {
+    std::string lastOp = getOperator();
+    popOperator();
+    applyOperator(lastOp);
+  }
 
-  // Connect origin to temp head through epsilon
-  this->graph.addEdge(std::to_string(origin), getTempHeadNode(), "ε");
+  std::cout << "\nAutomata: ";
+  printAutomatas();
+  std::cout << "\nOperators: ";
+  printOperators();
+  std::cout << std::endl;
+}
 
-  // Connect current tail to destiny through epsilon
-  this->graph.addEdge(this->graph.getTail(), std::to_string(destiny), "ε");
+// Private methods
 
-  // Connect origin to destiny through epsilon
-  this->graph.addEdge(std::to_string(origin), std::to_string(destiny), "ε");
-
-  // Set origin as temp head
-  setTempHeadNode(std::to_string(origin));
-
-  // Set destiny as tail
-  this->graph.setTail(std::to_string(destiny));
+// Push methods
+/**
+ * @brief
+ * Push an operator to the stack
+ * @param op
+ * The operator to push
+ */
+void Automata::pushOperator(std::string op)
+{
+  this->operators.push(op);
 }
 
 /**
  * @brief
- * Or operator. Chooses between two characters
- * @return void
+ * Push an automata to the stack
+ * @param automata
+ * The automata to push
  */
 
-void Automata::or_(std::string node1, std::string node2)
+void Automata::pushAutomata(Graph automata)
 {
-  // Pop operator
-  popOperator();
+  this->automatas.push(automata);
+}
 
-  // Create 4 new nodes
-  int origin, destiny;
+// Pop methods
+/**
+ * @brief
+ * Pop an automata from the stack
+ * @return Graph
+ * The automata popped
+ */
+Graph Automata::popAutomata()
+{
+  Graph automata = this->automatas.top();
+  this->automatas.pop();
+  return automata;
+}
 
-  origin = this->graph.createNode();
-  destiny = this->graph.createNode();
+/**
+ * @brief
+ * Pop an operator from the stack
+ * @return std::string
+ * The operator popped
+ */
+std::string Automata::popOperator()
+{
+  std::string op = this->operators.top();
+  this->operators.pop();
+  return op;
+}
 
-  // Set origin as head
-  if (getTempHeadNode() == this->graph.getHead())
-    this->graph.setHead(std::to_string(origin));
+// Getters
+/**
+ * @brief
+ * Get the automata from the stack
+ * @return Graph
+ * The automata
+ */
 
-  // Create new connection between origin and temp head through epsilon
-  this->graph.addEdge(std::to_string(origin), getTempHeadNode(), "ε");
+Graph Automata::getAutomata()
+{
+  return this->automatas.top();
+}
 
-  // Create new connection between origin and Node1 through epsilon
-  this->graph.addEdge(std::to_string(origin), node1, "ε");
+/**
+ * @brief
+ * Get the operator from the stack
+ * @return std::string
+ * The operator
+ */
 
-  // Create new connection between tail and destiny through epsilon
-  this->graph.addEdge(this->graph.getTail(), std::to_string(destiny), "ε");
+std::string Automata::getOperator()
+{
+  return this->operators.top();
+}
 
-  // Create new connection between Node2 and destiny through epsilon
-  this->graph.addEdge(node2, std::to_string(destiny), "ε");
+// Automata methods
+/**
+ * @brief
+ * Apply the operator
+ */
+void Automata::applyOperator(std::string op)
+{
+  if (op == "*")
+    std::cout << "Apply zero or more\n";
 
-  // Set origin2 as temp head
-  setTempHeadNode(std::to_string(origin));
+  if (op == "+")
+    std::cout << "Apply one or more\n";
 
-  // Set destiny2 as tail
-  this->graph.setTail(std::to_string(destiny));
+  if (op == CONCAT_OP)
+    concat(popAutomata(), popAutomata());
+
+  if (op == "|")
+    std::cout << "Apply or\n";
+
+  if (op == "(" | op == ")")
+    std::cout << "Apply parenthesis";
+}
+
+/**
+ * @brief
+ * Verify operator precedence weight
+ * @param op Operator
+ * @return int Weight
+ */
+int Automata::presedence(std::string op)
+{
+  if (op == "*")
+    return 5;
+  else if (op == "+")
+    return 4;
+  else if (op == ".")
+    return 3;
+  else if (op == "|")
+    return 2;
+  else if (op == "(" | op == ")")
+    return 1;
+  else
+    return -1;
+}
+
+// Printers
+
+/**
+ * @brief
+ * Print the automata
+ */
+void Automata::print()
+{
+  std::cout << "Automata: " << std::endl;
+}
+
+/**
+ * @brief
+ * Print the automatas
+ */
+void Automata::printAutomatas()
+{
+  std::stack<Graph> tempStack = automatas;
+
+  while (!tempStack.empty())
+  {
+    Graph element = tempStack.top();
+    tempStack.pop();
+    element.print();
+  }
+}
+
+/**
+ * @brief
+ * Print the operators
+ */
+void Automata::printOperators()
+{
+  std::stack<std::string> tempStack = operators; // Create a temporary stack
+
+  while (!tempStack.empty())
+  {
+    std::string element = tempStack.top(); // Get the top element
+    tempStack.pop();                       // Remove the top element
+    std::cout << element << " ";           // Print the element
+  }
+}
+
+// Operators
+
+/**
+ * @brief
+ * Concatenate two automatas
+ * @param automata1
+ * The first automata
+ * @param automata2
+ * The second automata
+ * @return Graph
+ * The concatenated automata
+ */
+
+Graph Automata::concat(Graph graph1, Graph graph2)
+{
+  graph1.join(graph2);
+  this->pushAutomata(graph1);
+  return graph1;
 }
